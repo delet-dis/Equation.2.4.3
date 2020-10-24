dbe13f45fc9395cc378abe3e8e48cb47180b82b6
Igor Efimov <efimov.036@gmail.com>
2020-10-24 15:51:55 +0700

    added non-working solving qu

--- app/src/main/java/ru/samsung/itschool/book/equation243/MainActivity.java
+++ app/src/main/java/ru/samsung/itschool/book/equation243/MainActivity.java
@@ -8,25 +8,44 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    // Вызывается при создании Активности
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Инициализирует Активность.
-        setContentView(R.layout.activity_main);
-    }
+  // Вызывается при создании Активности
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	// Инициализирует Активность.
+	setContentView(R.layout.activity_main);
+  }
 
-    /** Вызывается при нажатии пользователем на кнопку Решить */
-        public void solveEquation(View view) {
-             // ax+b=c
-            double a = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_a)).getText().toString());
-            double b = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_b)).getText().toString());
-            double c = Double.parseDouble( ((EditText)
-                    findViewById(R.id.coefficient_c)).getText().toString());
-            TextView result = (TextView) findViewById(R.id.result);
-            result.setText("" + (c - b) / a);
-        }
+  /**
+   * Вызывается при нажатии пользователем на кнопку Решить
+   */
+  public void solveEquation(View view) {
+	// ax+b=c
+	double a = Double.parseDouble(((EditText)
+			findViewById(R.id.coefficient_a)).getText().toString());
+	double b = Double.parseDouble(((EditText)
+			findViewById(R.id.coefficient_b)).getText().toString());
+	double c = Double.parseDouble(((EditText)
+			findViewById(R.id.coefficient_c)).getText().toString());
+	TextView result = (TextView) findViewById(R.id.result);
+
+	double discriminant = Math.pow(b, 2) - 4 * a * c;
+	double sqrt = Math.sqrt(discriminant);
+
+	double firstRoot;
+	double secondRoot;
+
+	String stringResult = "";
+
+	if (discriminant > 0) {
+	  firstRoot = (-b + sqrt) / (2 * a);
+	  secondRoot = (-b - sqrt) / (2 * a);
+	  stringResult = "Roots are :: " + firstRoot + " and " + secondRoot;
+	} else if (discriminant == 0) {
+	  stringResult = "Root is :: " + (-b + sqrt) / (2 * a);
+	}
+
+	result.setText(stringResult);
+  }
 
 }
